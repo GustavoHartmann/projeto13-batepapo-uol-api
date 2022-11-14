225a72edf359c5ab2d5673c6eb4463554d853743
Gustavo <gustavolerner.hartmann@gmail.com>
2022-11-14 14:29:23 -0300

    Feat: Post messages

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const participantsSchema = joi.object({
   name: joi.string().required(),
 });
 
+const messagesSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
+});
+
 const app = express();
 
 app.use(cors());
@@ -75,6 +81,44 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await collectionParticipants.find().toArray();
     res.send(participants);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+  const day = dayjs().format("HH:mm:ss");
+  const messageCreated = {
+    from: user,
+    to,
+    text,
+    type,
+    time: day,
+  };
+
+  try {
+    const { error } = messagesSchema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (error) {
+      const errors = error.details.map((d) => d.message);
+      res.status(422).send(errors);
+      return;
+    }
+    const participantExists = await collectionParticipants.findOne({
+      name: user,
+    });
+    if (!participantExists) {
+      res.status(422).send({ message: "Esse participante não existe!" });
+      return;
+    }
+
+    await collectionMessages.insertOne(messageCreated);
+    res.sendStatus(201);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
